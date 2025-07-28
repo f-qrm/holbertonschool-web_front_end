ad08d4305729184764ffb306f599705c10eeac21
f-qrm <qerimifjolla7@gmail.com>
2025-07-28 13:58:45 +0200

    style: documentation

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+This module defines and annotates variables of different basic types.
+
+It includes an integer, a float, a boolean, and a string, all properly
+annotated
+and assigned with their respective values for demonstration purposes.
+"""
+
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
